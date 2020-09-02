c21662a2025ab2c12cab06dacf8805f883b9cbfe
Alanio1 <70169920+Alanio1@users.noreply.github.com>
2020-09-02 00:50:17 -0500

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+/*
+Alanio Reese
+2020
+8 / 25 / 2020
+Lab 1 "Hello World"
+/**/
+#include <iostream>		
+
+int main()
+{
+	//My Favorite color is Black
+
+	std::cout << "My Favorite color is Black";
+
+	return 0;
+}
